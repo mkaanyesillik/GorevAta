c5562547867af66030aa842a5d26e0c6faab15de
Kaan Yeşillik <mkaanyesillik@gmail.com>
2021-09-05 01:49:57 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace GorevAta
+{
+    partial class KisayolProgramAtama
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(68, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Program Yolu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 42);
+            this.button2.Margin = new System.Windows.Forms.Padding(10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(68, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(490, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Program Yolu";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(564, 75);
+            this.button3.Margin = new System.Windows.Forms.Padding(10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Seç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(68, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(490, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Program Yolu";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(564, 106);
+            this.button4.Margin = new System.Windows.Forms.Padding(10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Seç";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(68, 111);
+            this.label4.Margin = new System.Windows.Forms.Padding(10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(490, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Program Yolu";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(564, 137);
+            this.button5.Margin = new System.Windows.Forms.Padding(10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Seç";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(68, 142);
+            this.label5.Margin = new System.Windows.Forms.Padding(10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(490, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Program Yolu";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(564, 168);
+            this.button6.Margin = new System.Windows.Forms.Padding(10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Seç";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(68, 173);
+            this.label6.Margin = new System.Windows.Forms.Padding(10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(490, 18);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Program Yolu";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(564, 198);
+            this.button7.Margin = new System.Windows.Forms.Padding(10);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Seç";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(68, 203);
+            this.label7.Margin = new System.Windows.Forms.Padding(10);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(490, 18);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Program Yolu";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "F12";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "F11";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 80);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "F10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(30, 111);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "F9";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(30, 142);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "F8";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(30, 173);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(19, 13);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "F7";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(30, 203);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(19, 13);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "F6";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 234);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(194, 41);
+            this.button8.TabIndex = 29;
+            this.button8.Text = "Ayarla";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // KisayolProgramAtama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 289);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "KisayolProgramAtama";
+            this.Text = "Kısayola Program Atama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,305 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GorevAta
+{
+    public partial class KisayolProgramAtama : Form
+    {
+
+        public KisayolProgramAtama()
+        {
+            InitializeComponent();
+
+        }
+        [DllImport("user32.dll", CharSet = CharSet.Ansi)]
+        public static extern short GetAsyncKeyState(int Tus); // GetAsyncKeyState windows apisi gönderilen tuşa basılıp basılmadığını kontrol eder (ister uygulama için ister uygulama dışı farketmez.)
+
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr WindowHandle);
+        public const int SW_RESTORE = 9;
+
+        string dosya1;
+        string dosya2;
+        string dosya3;
+        string dosya4;
+        string dosya5;
+        string dosya6;
+        string dosya7;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string dosya_yolu = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\kısayollar.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            StreamReader sw = new StreamReader(fs);
+            string yazi = sw.ReadLine();
+            while (yazi != null)
+            {
+                string[] labeldata = yazi.Split(',');
+                if (labeldata[0] == "F12")
+                {
+                    label1.Text = labeldata[1];
+                }
+                if (labeldata[0] == "F11")
+                {
+                    label2.Text = labeldata[1];
+                }
+                if (labeldata[0] == "F10")
+                {
+                    label3.Text = labeldata[1];
+                }
+                if (labeldata[0] == "F9")
+                {
+                    label4.Text = labeldata[1];
+                }
+                if (labeldata[0] == "F8")
+                {
+                    label5.Text = labeldata[1];
+                }
+                if (labeldata[0] == "F7")
+                {
+                    label6.Text = labeldata[1];
+                }
+                if (labeldata[0] == "F6")
+                {
+                    label7.Text = labeldata[1];
+                }
+                yazi = sw.ReadLine();
+
+            }
+            sw.Close();
+            fs.Close();
+
+        }
+        public static bool TusKontrol(byte Kod) // Windows apisini kullanarak tuş kontrolü yapar.
+        {
+            if (GetAsyncKeyState((int)Kod) == System.Int16.MinValue + 1)
+                return true;
+            else
+                return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (TusKontrol((byte)Keys.F12))
+            {
+                if (dosya1 == "")
+                {
+
+                }
+                else
+                {
+                    Process.Start(dosya1);
+                }
+
+            }
+            if (TusKontrol((byte)Keys.F11))
+            {
+                if (dosya2 == "")
+                {
+
+                }
+                else
+                {
+                    Process.Start(dosya2);
+                }
+
+            }
+            if (TusKontrol((byte)Keys.F10))
+            {
+                if (dosya3 == "")
+                {
+
+                }
+                else
+                {
+                    Process.Start(dosya3);
+                }
+
+            }
+            if (TusKontrol((byte)Keys.F9))
+            {
+                if (dosya4 == "")
+                {
+
+                }
+                else
+                {
+                    Process.Start(dosya4);
+                }
+
+            }
+            if (TusKontrol((byte)Keys.F8))
+            {
+                if (dosya5 == "")
+                {
+
+                }
+                else
+                {
+                    Process.Start(dosya5);
+                }
+
+            }
+            if (TusKontrol((byte)Keys.F7))
+            {
+                if (dosya6 == "")
+                {
+
+                }
+                else
+                {
+                    Process.Start(dosya6);
+                }
+
+            }
+            if (TusKontrol((byte)Keys.F6))
+            {
+                if (dosya7 == "")
+                {
+
+                }
+                else
+                {
+                    Process.Start(dosya7);
+                }
+
+            }
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Uygulama Dosyaları (*.exe)|*.exe";
+            ofd.ShowDialog();
+            label1.Text = ofd.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Uygulama Dosyaları (*.exe)|*.exe";
+            ofd.ShowDialog();
+            label2.Text = ofd.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Uygulama Dosyaları (*.exe)|*.exe";
+            ofd.ShowDialog();
+            label3.Text = ofd.FileName;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Uygulama Dosyaları (*.exe)|*.exe";
+            ofd.ShowDialog();
+            label4.Text = ofd.FileName;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Uygulama Dosyaları (*.exe)|*.exe";
+            ofd.ShowDialog();
+            label5.Text = ofd.FileName;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Uygulama Dosyaları (*.exe)|*.exe";
+            ofd.ShowDialog();
+            label6.Text = ofd.FileName;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Uygulama Dosyaları (*.exe)|*.exe";
+            ofd.ShowDialog();
+            label7.Text = ofd.FileName;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (label1.Text != "Program Yolu")
+            {
+
+                dosya1 = label1.Text;
+            }
+            if (label2.Text != "Program Yolu")
+            {
+
+                dosya2 = label2.Text;
+            }
+            if (label3.Text != "Program Yolu")
+            {
+
+                dosya3 = label3.Text;
+            }
+            if (label4.Text != "Program Yolu")
+            {
+
+                dosya4 = label4.Text;
+            }
+            if (label5.Text != "Program Yolu")
+            {
+
+                dosya5 = label5.Text;
+            }
+            if (label6.Text != "Program Yolu")
+            {
+
+                dosya6 = label6.Text;
+            }
+            if (label7.Text != "Program Yolu")
+            {
+
+                dosya7 = label7.Text;
+            }
+            File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\kısayollar.txt");
+            string dosya_yolu = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\kısayollar.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("F12," + dosya1);
+            sw.WriteLine("F11," + dosya2);
+            sw.WriteLine("F10," + dosya3);
+            sw.WriteLine("F9," + dosya4);
+            sw.WriteLine("F8," + dosya5);
+            sw.WriteLine("F7," + dosya6);
+            sw.WriteLine("F6," + dosya7);
+
+
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+        }
+
+
+    }
+}
